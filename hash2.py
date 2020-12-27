3bc01b47cc0ca849caa5e2c5942530c72d6874a5
28tad <71690649+28tad@users.noreply.github.com>
2020-12-27 17:41:22 +0400

    Create hash2.py

---
+++ hash2.py
@@ -0,0 +1,24 @@
+import uuid
+import hashlib
+
+
+def hash_password(password):
+    # uuid используется для генерации случайного числа
+    salt = uuid.uuid4().hex
+    return hashlib.sha256(salt.encode() + password.encode()).hexdigest() + ':' + salt
+
+
+def check_password(hashed_password, user_password):
+    password, salt = hashed_password.split(':')
+    return password == hashlib.sha256(salt.encode() + user_password.encode()).hexdigest()
+
+
+new_pass = input('Enter your password: ')
+hashed_password = hash_password(new_pass)
+print('String to store in the database: ' + hashed_password)
+old_pass = input('Enter the password again for verification: ')
+
+if check_password(hashed_password, old_pass):
+    print('You entered the correct password')
+else:
+    print('Sorry, but the passwords don not match')
