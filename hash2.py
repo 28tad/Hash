1974c048ae602fd945ee44f7aeea1d5cd871444e
28tad <71690649+28tad@users.noreply.github.com>
2020-12-27 17:46:19 +0400

    Update hash2.py

--- hash2.py
+++ hash2.py
@@ -3,7 +3,7 @@ import hashlib
 
 
 def hash_password(password):
-    # uuid используется для генерации случайного числа
+    # uuid used to generate a random number
     salt = uuid.uuid4().hex
     return hashlib.sha256(salt.encode() + password.encode()).hexdigest() + ':' + salt
 
